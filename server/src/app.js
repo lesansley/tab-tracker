cbbb37e4b33fa4bf52ebec055fed67199394f12d
Les Ansley <les@ansleyfamily.com>
2018-01-12 16:14:18 +0200

    Add CORS handling withour cors npm module

--- server/src/app.js
+++ server/src/app.js
@@ -1,15 +1,27 @@
 const express = require('express');
 
 const bodyParser = require('body-parser');
-const cors = require('cors');
 const morgan = require('morgan');
 
 const app = express();
 
 app
   .use(morgan('combined'))
-  .use(bodyParser.json())
-  .use(cors());
+  .use(bodyParser.urlencoded({extended: false}))
+  .use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow_Origin', '*');
+  res.header(
+    'Access-COntrol-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  if (res.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'POST');
+    return res.status(200).json({});
+  }
+  next();
+});
 
 require('./routes')(app);
 
